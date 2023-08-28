5a2d1d01fb26f78869777f94caf2d31de45c5a39
Afaq Ahmad <you@example.com>
2023-08-28 16:39:02 +0500

    refeactor Co-authored-by: name <huzaifa.saleem311@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 app.listen(3000, () => {
-	console.log(`Example app listening at http://localhost:${3000}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
