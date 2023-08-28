5250219b401e868925d4c5fc29138a67088c4f08
Afaq Ahmad <you@example.com>
2023-08-28 17:08:15 +0500

    new changes Co-authored-by: Huzaifa Saleem <huzaifa.saleem3111@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.get('/docs', (req, res) => {
-	res.send('Hello docs!');
-});
-
 app.listen(3000, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
