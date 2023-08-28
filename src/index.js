cb8d10427cd26ed67e64962a1644d158304680f3
Afaq Ahmad <you@example.com>
2023-08-28 16:49:54 +0500

    new changes in dev branch Co-authored-by: huzaifa-saleem <huzaifa.saleem3111@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const port = 3000;
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+
+app.get('/docs', (req, res) => {
+	res.send('Hello docs!');
+});
+
 app.listen(3000, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
