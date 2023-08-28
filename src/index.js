af61c6f8a6f090648d862fc18eb0b5718c2385b8
Afaq Ahmad <you@example.com>
2023-08-28 16:36:56 +0500

    new updated code Co-authored-by: name malikafaqahmad4455@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+	console.log(`Example app listening at http://localhost:${3000}`);
 });
